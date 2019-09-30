9bf2c33b1667886e602192822422f46c4a3b6230
6010451078 <numin123@gmail.com>
2019-09-30 20:38:49 +0700

    fix login and register

--- resources/views/login/register.blade.php
+++ resources/views/login/register.blade.php
@@ -2,24 +2,33 @@
 @section('content')   
 
 <form name="form1" method="post" action="check_login.php">
+<div class="card mx-auto" style="width: 50%">
+  <div class="card-body mx-auto">
   สมัครสมาชิก<br>
   <table border="1" style="width: 300px">
     <tbody>
       <tr>
-        <td> &nbsp;Username</td>
+        <td> &nbsp;อีเมล</td>
         <td>
           <input name="txtUsername" type="text" id="txtUsername">
         </td>
       </tr>
       <tr>
-        <td> &nbsp;Password</td>
+        <td> &nbsp;รหัสผ่าน</td>
+        <td><input name="txtPassword" type="password" id="txtPassword">
+        </td>
+      </tr>
+      <tr>
+        <td> &nbsp;ยืนยันรหัสผ่าน</td>
         <td><input name="txtPassword" type="password" id="txtPassword">
         </td>
       </tr>
     </tbody>
   </table>
   <br>
-  <input type="submit" name="Submit" value="Login">
+  <input type="submit" name="Submit" value="สมัครสมาชิก">
+  </div>
+  </div>
 </form>
 
 @endsection
