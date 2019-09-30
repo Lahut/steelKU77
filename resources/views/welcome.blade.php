5fa79422c911828d5dbbccb2dd2d9ec5f41571a5
6010451078 <numin123@gmail.com>
2019-09-30 22:53:44 +0700

    add home img v2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,8 +4,35 @@
     <br><br>
     <img src="imghome/mp.jpg" class="rounded float-left" alt="..." height="40%" width="40%">
 <img src="imghome/sp.jpg" class="rounded float-right" alt="..." height="40%" width="40%">
-<br><br><br><br><br><br><br><br><br><br><br><br><br><br>
-    <h3>งานสำคัญ</h3>
+<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+<div class="card mb-3" style="max-width: 100%;">
+  <div class="row no-gutters">
+    <div class="col-md-4">
+      <img src="imghome/different-metal-products-stainless-steel-profiles-PD9SKW3-1024x768.jpg" class="card-img" alt="...">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">จำหน่ายเหล็กรุปพรรณ เกรด A และ B ทุกชนิด</h5>
+        <p class="card-text">ลักษณะเหล็กเกรด  A    เป็นเหล็กใหม่ จากโรงงานผลิต  เรียบ ตรง ไม่มีรอยต่อ
+ลักษณะเหล็กเกรด B     เป็นเหล็กที่มาจากโรงงานผลิต แต่ไม่สมบูรณ์ เช่นความยามไม่ถึง 6 ม.  เหล็กมีรอยต่อ ตำหนิ  แต่ไม่ใช่เหล็กเก่า</p>
+        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="card mb-3" style="max-width: 100%;">
+  <div class="row no-gutters">
+    <div class="col-md-4">
+      <img src="imghome/different-metal-products-stainless-steel-profiles-PD9SKW3-1024x768.jpg" class="card-img" alt="...">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">จำหน่ายเหล็กรุปพรรณ เกรด A และ B ทุกชนิด</h5>
+        <p class="card-text">ลักษณะเหล็กเกรด  A    เป็นเหล็กใหม่ จากโรงงานผลิต  เรียบ ตรง ไม่มีรอยต่อ
+ลักษณะเหล็กเกรด B     เป็นเหล็กที่มาจากโรงงานผลิต แต่ไม่สมบูรณ์ เช่นความยามไม่ถึง 6 ม.  เหล็กมีรอยต่อ ตำหนิ  แต่ไม่ใช่เหล็กเก่า</p>
+        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+      </div>
+    </div>
+  </div>
+</div>
 @endsection
-
-
